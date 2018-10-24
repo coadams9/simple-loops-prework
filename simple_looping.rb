0861898554c550fbdb2fe7893301758824cdb470
Cory Adams <coadams9@gmail.com>
2018-10-24 21:20:07 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -41,7 +41,7 @@ end
 def for_iterator(number_of_times)
 
 phrase = "Welcome to Flatiron School's Web Development Course!"  
-  for item in number_of_times do
+  for phrase in number_of_times do
     puts phrase
   end
 end
