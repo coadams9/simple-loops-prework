97203cb71c06f0dc445c39650eed218d9e81e812
Cory Adams <coadams9@gmail.com>
2018-10-24 20:08:26 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 counter = 0
 def loop_iterator(number_of_times)
-  
+  phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
     puts phrase
     counter += 1
